db03eda312a84f62cea3d11539782a00d502846d
unknown <mahlledz@gmail.com>
2022-08-22 11:47:19 +0100

    generational index

--- main.cs
+++ main.cs
@@ -5,10 +5,19 @@ class MainClass
 {
     static void Main(string[] args)
     {
-        Console.Write("Main");
+        Console.Write("Main" + "\n");
 
         ColorPaleteMap palette = new ColorPaleteMap();
         palette.Initialize();
+
+        Int64 id = Utils.CreateID(10, 20);
+        Int32 time = Utils.GetTimeFromID(id);
+        Int32 randomNumber = Utils.GetRandomNumberFromID(id);
+
+        Console.Write(id + "\n");
+        Console.Write(time + "\n");
+        Console.Write(randomNumber + "\n");
+
 
         //palette.GetHorizontalColor(4);
     }
--- utils.cs
+++ utils.cs
@@ -7,5 +7,28 @@ namespace wang
         {
             return r << 24 + g << 16 + b << 8 + a;
         }
+
+        public static Int64 CreateID(Int32 timeInSeconds, Int32 randomNumber)
+        {
+            Int64 result = timeInSeconds << 32 + randomNumber;
+
+            return result;
+        }
+
+        public static Int32 GetTimeFromID(Int64 id)
+        {
+            return (Int32)(id >> 32);
+        }
+
+        public static Int32 GetRandomNumberFromID(Int64 id)
+        {
+            return (Int32)((id << 32) >> 32);
+        }
+
+        public static string IDToString(Int64 id)
+        {
+            //TODO(Mahdi): empty for now
+            return "";
+        }
     }
 }
