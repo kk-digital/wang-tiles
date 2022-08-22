89e03c909f644c3dba8b6182b52539aed806f5ee
Choi <ridvansu@hotmail.com>
2022-08-22 18:52:46 +0300

    Edge Tile Set Functions Implemented
    Edge Tile Set Functions Implemented.
    Calculations Implemented.
    List Commands Added.

--- EdgeTileSet.cs
+++ EdgeTileSet.cs
@@ -1,5 +1,8 @@
 using System.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
 using wang_tiles;
+using System.Linq;
 
 namespace wang_tiles
 {
@@ -12,13 +15,20 @@ namespace wang_tiles
         public int VerticalColorsCount;
         public int HorizontalColorsCount;
         public TileInformation[] InformationArray;
-
+        public int TileIndex;
+        public int[] TileLookUpArray;
+    
 
         
 
         public void InitEdgeTileSet()
         {
             // TODO(Mahdi): Implement
+
+            // Create and Init Variables
+            InformationArray = new TileInformation[16];
+            VerticalColorsCount = 0;
+            HorizontalColorsCount = 0;
         }
 
 
@@ -29,13 +39,37 @@ namespace wang_tiles
             Debug.Assert(rightColor >= -1 && rightColor < VerticalColorsCount);
             Debug.Assert(leftColor >= -1 && leftColor < VerticalColorsCount);
 
+            VerticalColorsCount++;
+            HorizontalColorsCount++;
+            
             // TODO(Mahdi): Implement
+
+            // Initialize Information Array
+            TileInformation properties = new TileInformation
+            {
+                TileID = -1,
+                TopColor = topColor,
+                BottomColor = bottomColor,
+                LeftColor = leftColor,
+                RightColor = rightColor
+            };
+            InformationArray.Append(properties);
+
+            TileLookUpArray = new int[HorizontalColorsCount * VerticalColorsCount];
+            
+
+            TileIndex = leftColor * (HorizontalColorsCount * VerticalColorsCount) + rightColor * (VerticalColorsCount * VerticalColorsCount) +
+                            bottomColor * VerticalColorsCount + topColor;
+
+            TileLookUpArray.Append(TileIndex);
 
         }
 
         public void FinalizeTileSet()
         {
             // TODO(Mahdi): Implement
+
+
         }
     }
 }
