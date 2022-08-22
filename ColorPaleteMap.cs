74931d88b0789c85fd4630fb3f84c89ef4b9f2dd
Choi <ridvansu@hotmail.com>
2022-08-22 12:33:19 +0300

    Color Palete Map Colors Added
    Color Palete Map Colors Added

--- ColorPaleteMap.cs
+++ ColorPaleteMap.cs
@@ -1,3 +1,6 @@
+
+using System.Security.Cryptography;
+
 namespace wang
 {
     public class ColorPaleteMap
@@ -7,12 +10,36 @@ namespace wang
 
         public PixelColor GetHorizontalColor(int index)
         {
+            if(index <= 0)
+                throw new ArgumentOutOfRangeException("Out Of Range");
+
             return HorizontalEdgeColors[index];
         }
 
         public PixelColor GetVerticalColor(int index)
         {
+            if(index <= 0)
+                throw new ArgumentOutOfRangeException("Out Of Range");
+
             return VerticalEdgeColors[index];
+        }
+
+        public void Initialize()
+        {
+            Random random = new Random();
+
+            HorizontalEdgeColors = new PixelColor[16];
+            VerticalEdgeColors = new PixelColor[16];
+
+            for(int i = 0; i < HorizontalEdgeColors.Length; i++)
+            {
+                HorizontalEdgeColors[i] = PixelColor.MakePixelColor(random.Next(0,255), random.Next(0,255), random.Next(0,255), 255);
+            }
+
+            for(int i = 0; i < VerticalEdgeColors.Length; i++)
+            {
+                VerticalEdgeColors[i] = PixelColor.MakePixelColor(random.Next(0,255), random.Next(0,255), random.Next(0,255), 255);
+            }
         }
     }
 }
