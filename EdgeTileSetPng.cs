18a27cc2c8751c5af23e4143bd843912704fc415
unknown <mahlledz@gmail.com>
2022-08-22 11:38:15 +0100

    edge tileset

---
+++ EdgeTileSetPng.cs
@@ -0,0 +1,17 @@
+using System.Diagnostics;
+
+namespace wang
+{
+
+
+    // TODO(Mahdi): https://github.com/kk-digital/kcg/issues/567
+    public partial class EdgeTileSet
+    {
+
+
+        public void SavePNG(string filename)
+        {
+            // TODO(Mahdi): Implement
+        }
+    }
+}
