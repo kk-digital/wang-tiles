9f229252f0c0e13894b5bc6a31a8fb1dcf7801da
Choi <ridvansu@hotmail.com>
2022-08-26 14:23:06 +0300

    Assigning Sprites to BoardTile (AKA in Scene) Implementation Removed
    Assigning Sprites to BoardTile (AKA in Scene) Implementation Removed

--- main.cs
+++ main.cs
@@ -12,10 +12,6 @@ class MainClass
 {
     static void Main(string[] args)
     {
-        Scene scene = Scene.Algorithm1(TileSize.TileSize_16x16);
-
-        scene.TileSets[0].SavePNG("MatchingCornerTileset.png", 8);
-
         CommandLine.ExecuteCommand(args);
     }
 }
