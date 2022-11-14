b5ae76454eaaf8ce1dc2db83589b0e13f87f86da
Kenje Hofileña <kenje4090@gmail.com>
2022-11-14 16:01:22 +0800

    Add color enum 1 as air, other type

--- Source2/Wang/Color.cs
+++ Source2/Wang/Color.cs
@@ -4,8 +4,10 @@ namespace Wang
     {
         // MatchAll means match anything
         MatchAll = 0,
+        Air, // Color 1 is air, space, or other
         Red,
         Green,
         Blue,
     }
+
 }
