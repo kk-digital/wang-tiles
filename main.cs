ee8025ddd53f79766a950b378c13fe8c7e339469
Kenje Hofileña <kenje4090@gmail.com>
2022-11-15 17:52:41 +0800

    Add PlacementAlgo_V1

--- main.cs
+++ main.cs
@@ -1,17 +1,15 @@
 
 using Wang;
-using Wang.Other;
-using Wang.SceneW;
-using Wang.Board;
-using Wang.CLI;
-using Wang.EdgeTile;
+
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
 class MainClass
 {
     static void Main(string[] args)
-    {
-        CommandLine.ExecuteCommand(args);
+    {   
+        Generator newGeneratedBoard= new Generator();
+        newGeneratedBoard.PlacementAlgo_V1();
+       
     }
 }
